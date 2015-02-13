1a25a584077090adb6dc6e17ede1f807c7d582f6
winkidney <winkidney@gmail.com>
2015-02-13 22:27:33 +0800

    update for python wheel

--- setup.py
+++ setup.py
@@ -3,50 +3,50 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
+with open(os.path.join(here, 'README.txt')) as f:
     README = f.read()
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
-requires = [
-    'bcrypt',
-]
-complex_requires = [
-    'sqlalchemy',
-    'wtforms',
-    'psycopg2',
-    'pycrypto',
-    'padding',
-    'jsonschema',
-]
+requires = []
+complex_requires = {
+    "forms": [
+        'wtforms',
+        'jsonschema',
+    ],
+    'security': ['bcrypt', ],
+    'sqlalchemy': ['sqlalchemy', 'psycopg2', 'pycrypto', ],
+}
 test_requires = [
     'nose',
     'coverage',
 ]
 
-setup(name='web_utils',
-      version='0.0.1',
-      description='web_utils collection that used in web development process.',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Topic :: Utilities",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Software Development :: Libraries",
-      ],
-      author='winkidney',
-      author_email='winkidney@gmail.com',
-      url='http://github.com/winkidney/web_utils',
-      keywords='web development utils ',
-      packages=find_packages(),
-      package_data={
-          '': ['README.md', 'run_tests.sh'],
-      },
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires+complex_requires+test_requires,
-      tests_require=test_requires,
-      test_suite="web_utils",
-      license="GPLv3",
-      platform="any",
-      )
+setup(
+    name='web_utils',
+    version='0.0.2',
+    description='web_utils collection that used in web development process.',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+    ],
+    author='winkidney',
+    author_email='winkidney@gmail.com',
+    url='http://github.com/winkidney/web_utils',
+    keywords='web development utils ',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.txt', '*.sh', '*.md'],
+    },
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=complex_requires,
+    tests_require=test_requires,
+    test_suite="web_utils",
+    license="GPLv3",
+    platform="any",
+)
